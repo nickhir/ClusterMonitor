1160353c9286234f0888afe9c1ff53c9c99de4a1
nickhir <nick.hirschmueller@yahoo.de>
2021-06-12 22:58:05 +0200

    removed useless function

--- cpu_ram_log.py
+++ cpu_ram_log.py
@@ -6,17 +6,6 @@ import csv
 import os
 import datetime
 
-
-def convert_size(size_bytes):
-    if size_bytes == 0:
-        return "0B"
-    size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
-    i = int(math.floor(math.log(size_bytes, 1024)))
-    p = math.pow(1024, i)
-    s = round(size_bytes / p, 2)
-    return "%s %s" % (s, size_name[i])
-
-
 p = argparse.ArgumentParser()
 p.add_argument("-u", "--username", type=str, help="Specify the username for which we record CPU and Memory usage")
 p.add_argument("-o", "--output", type=str,
